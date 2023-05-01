60cec811f17320e283d4ae1f0b8b0bfb79413a4d
DontLikenaming <edirna21@gmail.com>
2023-05-01 15:47:21 +0900

    회원가입 - 이용약관 페이지

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -1,4 +1,29 @@
+// header 로고 클릭 이벤트
 let logo = document.querySelector("#logo");
 logo.addEventListener("click", function (){
+    location.href="/";
+})
+
+// 이용약관 체크박스
+let agree1 = document.querySelector("#agree1");
+
+
+
+// 개인정보 이용 체크박스
+let agree2 = document.querySelector("#agree2");
+
+
+
+// 동의 버튼 이벤트
+let agreeok = document.querySelector("#agreeok");
+agreeok?.addEventListener("click", () => {
+    if(!agree1.checked) alert("이용약관 동의에 체크해주세요!")
+    else if(!agree2.checked) alert("개인정보 이용 동의에 체크해주세요!")
+    else location.href="/join/checkme";
+})
+
+// 미동의 버튼 이벤트
+let noagree = document.querySelector("#noagree");
+noagree?.addEventListener("click", () => {
     location.href="/";
 })
