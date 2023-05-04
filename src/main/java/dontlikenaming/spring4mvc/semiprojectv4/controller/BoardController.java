bec6bd8ab17198f450cd52b69d60f1934fc5f3c7
DontLikenaming <edirna21@gmail.com>
2023-05-04 10:41:04 +0900

    게시판, 자료실, 갤러리 controller 추가 및 일부 수정

---
+++ src/main/java/dontlikenaming/spring4mvc/semiprojectv4/controller/BoardController.java
@@ -0,0 +1,15 @@
+package dontlikenaming.spring4mvc.semiprojectv4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/board")
+public class BoardController {
+
+    @GetMapping(value = "/list")
+    public String list(){
+        return "board/list.tiles";
+    }
+}
--- src/main/webapp/WEB-INF/views/admin.jsp
+++ src/main/webapp/WEB-INF/views/admin.jsp
@@ -1,5 +1,5 @@
 <%@ page pageEncoding="UTF-8" %>
 <div class="mt-4">
-    <i class="fa-solid fa-screwdriver-wrench fa-2xl"></i> 관리자</i>
+    <i class="fa-solid fa-screwdriver-wrench fa-2xl"> 관리자</i>
     <hr>
 </div>
